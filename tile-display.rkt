4c294a0a7a01baa4f7f6dee2bf7a3b323cf27b0f
Jokes <et.irrumabo@gmail.com>
2019-12-13 21:04:13 -0500

    tile-display does more things now but "work" is still not one of the things

--- tile-display.rkt
+++ tile-display.rkt
@@ -5,22 +5,39 @@
 (define phi 1.6180339887498948482)
 
 (define (draw-zero dc x y s) ; the diamond
-  (error))
+  (let ([ps (* phi s)])
+    (send dc draw-line x (- y ps) (+ x s) y) ; north to east
+    (send dc draw-line (+ x s) y x (+ y ps)) ; east to south
+    (send dc draw-line x (+ y ps) (- x s) y) ; south to west
+    (send dc draw-line (- x s) y x (- y ps)) ; west to north
+    ))
 
 (define (draw-one dc x y s) ; vertical central line
-  (error))
-(define (draw-two dc x y s) ; N figure
-  (error))
+  (let ([ps (* phi s)])
+    (send dc draw-line x (- y ps) x (+ y ps))))
 
-(define (draw-four dc x y h) ; horizontal central line
-  (error))
+(define (draw-two dc x y s) ; N figure
+  (let ([hp (/ (* phi s) 2)])
+    (send dc draw-line (- x hp) (+ y hp) (- x hp) (- y hp)) ; southwest to northwest
+    (send dc draw-line (- x hp) (- y hp) (+ x hp) (+ y hp)) ; northwest to southeast
+    (send dc draw-line (+ x hp) (+ y hp) (+ x hp) (- y hp)) ; southeast to northeast
+    ))
+
+(define (draw-four dc x y s) ; horizontal central line
+  (let ([ps (* phi s)])
+  (send dc draw-line (+ x s) y) (- x s) y))
 (define (draw-eight dc x y s) ; horizontal parallels
-  (error))
+  (let ([hp (/ (* phi s) 2)])
+  (send dc draw-line (- x hp) (- y hp) (+ x hp) (- y hp)) ; northwest to northeast
+  (send dc draw-line (- x hp) (+ y hp) (+ x hp) (+ y hp)) ; southwest to southeast
+    ))
 
 (define (draw-16 dc x y s) ; diagonal lower left
-  (error))
+  (let ([hp (/ (* phi s) 2)])
+  (send dc draw-line (- x hp) (+ y hp) x y)))
 (define (draw-32 dc x y s) ; diagonal upper right
-  (error))
+  (let ([hp (/ (* phi s) 2)])
+  (send dc draw-line x y (+ x hp) (- y hp))))
 
 ; and put it all together...
 (define (draw-digit d dc x y s)
@@ -45,7 +62,10 @@
 
 
 ; canvasing things
+(define the-grid
+  (build-vector 8 (λ (n) (build-vector 8 (λ (m) (+ (* n 8) m))))))
 (define (draw-grid dc)
+  ; draw the grid
   (error))
 
 
